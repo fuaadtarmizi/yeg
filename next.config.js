2b4bcff8a103870e0221b29cd4bad3e62aea8227
fuaadtarmizi <fuaad2120@gmail.com>
2024-01-01 22:13:35 +0800

    test5

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
-// next.config.js
-const path = require('path');
-
-module.exports = {
-  // Specify the output directory for the build
-  distDir: 'build',
-
-  target: 'experimental-serverless-trace',
-};
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: 'export',
+   
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // trailingSlash: true,
+   
+    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+    // skipTrailingSlashRedirect: true,
+   
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
+  }
+   
+  module.exports = nextConfig
