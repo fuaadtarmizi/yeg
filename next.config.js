bdb233be54a6e526709289bf9715541cae3ef40e
fuaadtarmizi <fuaad2120@gmail.com>
2024-01-01 21:49:45 +0800

    test3

--- next.config.js
+++ next.config.js
@@ -1,10 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    images: {
-      loader: 'custom',
-      loaderFile: './my-loader.ts',
-    },
-  }
-   
-  module.exports = nextConfig
+// next.config.js
+const path = require('path');
+
+module.exports = {
+  // Specify the output directory for the build
+  distDir: 'build',
+
+  // Customize the export path map (if needed)
+  // exportPathMap: async function () {
+  //   return {
+  //     '/': { page: '/' },
+  //   };
+  // },
+
+  // Example: Configure custom image loader
+  images: {
+    loader: 'custom',
+    // Replace 'path/to/my-loader.ts' with the actual path to your custom image loader file
+    loaderFile: path.resolve('./path/to/my-loader.ts'),
+  },
+
+  // Add other configuration options as needed
+};
