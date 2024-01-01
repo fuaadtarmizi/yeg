54b0d149e94b2607f05a7db24e1458c0c3fcace7
fuaadtarmizi <fuaad2120@gmail.com>
2024-01-01 22:04:43 +0800

    test5

--- next.config.js
+++ next.config.js
@@ -5,13 +5,5 @@ module.exports = {
   // Specify the output directory for the build
   distDir: 'build',
 
-  // Customize the export path map (if needed)
-  // exportPathMap: async function () {
-  //   return {
-  //     '/': { page: '/' },
-  //   };
-  // },
-
-  
-  // Add other configuration options as needed
+  target: 'experimental-serverless-trace',
 };
