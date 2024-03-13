a5b4507d2f181f7b93872b95bb66549b3595fbda
fuaadtarmizi <fuaad2120@gmail.com>
2024-03-13 11:05:45 +0800

    test98

--- src/app/landingpage/hajumrah/melaka/dhumymqa2/page.js
+++ src/app/landingpage/hajumrah/melaka/dhumymqa2/page.js
@@ -1,32 +1,13 @@
 import React from 'react'
 import Image from 'next/image'
 import Formblastingdhumyganeral from '@/components/Formblastingdhumy2'
-import Head from 'next/head'
+import FacebookPixelDhumy from '@/components/FacebookPixelDhumy'
 
 
 function page() {
   return (
     <>
-   <Head>
-        {/* Facebook Pixel Code */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '527799262643186');
-              fbq('track', 'PageView');
-            `,
-          }}
-        />
-        {/* End Facebook Pixel Code */}
-      </Head>
+    <FacebookPixelDhumy/>
     <div>
       <div className="bg-slate-700">
       <div className="py-2 md:py-3 lg:py-6"></div>
