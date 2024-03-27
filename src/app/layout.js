3e1c25273275fdd5987cc6aa841b43c0608f4c7b
fuaadtarmizi <fuaad2120@gmail.com>
2024-03-27 11:17:14 +0800

    test135

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,20 +28,29 @@ export default function RootLayout({ children }) {
           <meta name="twitter:image" content="URL_TO_IMAGE" />
           <link rel="icon" href="./yeglogowebsite.svg" sizes="16x16" type="image/svg+xml" />
 
+
+          <Script
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  !function(f,b,e,v,n,t,s)
+                  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                  n.queue=[];t=b.createElement(e);t.async=!0;
+                  t.src=v;s=b.getElementsByTagName(e)[0];
+                  s.parentNode.insertBefore(t,s)}(window, document,'script',
+                  'https://connect.facebook.net/en_US/fbevents.js');
+                  fbq('init', '527799262643186');
+                  fbq('track', 'PageView');
+                `,
+              }}
+            />
+
+
           
 
-        {/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-0BWVHPJF8V"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', 'G-0BWVHPJF8V');
-            `,
-          }}
-        ></script> */}
+        
         {/* Add Clarity script here */}
         <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `
           (function(c,l,a,r,i,t,y){
