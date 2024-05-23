cfd128d3daa44ac8df5ccf68a38256c7f40d2797
fuaadtarmizi <fuaad2120@gmail.com>
2024-05-23 12:08:11 +0800

    test166

--- src/app/landingpage/ticketmmp/page.js
+++ src/app/landingpage/ticketmmp/page.js
@@ -1,33 +1,81 @@
-'use client'
 import React from 'react'
 import Image from 'next/image'
-import SliderJelajahAcm from '@/components/kembarahaji/SliderJelajahAcm'
-import Testimoni from '@/components/kembarahaji/Testimoni'
-import VideoMqa from '@/components/kembarahaji/VideoMqa'
-import Moto from '@/components/kembarahaji/Moto'
-import Footer from '@/components/Footer'
-import FormMmp from '@/components/FormMmp'
 import Countdown from '@/components/Countdown'
+import Footer from '@/components/Footer'
+import Faqticketmmp from '@/components/Faqticketmmp'
+
 
 
 function page() {
-  const initialTargetDate = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000); // Set your target date
-  
+
+  const initialTargetDate = new Date(Date.now() + 17 * 24 * 60 * 60 * 1000); 
+                                            
   return (
     <>
     <main>
+      {/* <section>
+      <div className="">
+        <video
+        controls
+        autoPlay  // Add this attribute for autoplay
+      >
+        <source src="/TEASERmmp.mp4" type="video/mp4" />
+      </video>
+        </div>
+      </section> */}
+      
       <section>
-        <div className="">
-          <SliderJelajahAcm/>
+      <div className="flex items-center">
+        <Image 
+            className="w-full"
+            src="/ticketmpp.jpg"
+            alt="kembarahaji1"
+            width="90"
+            height="200"
+            style={{
+              objectFit: "contain",
+            }}
+          />
         </div>
       </section>
-      <div className="bg-green-600 py-4 lg:py-10 md:[height:80px]"></div>
+      <div className="py-4 lg:py-10 md:[height:80px]"></div>
       <section>
-        <div className="bg-green-600">
-          <VideoMqa/>
+        <div>
+          <Countdown initialTargetDate={initialTargetDate} />
         </div>
       </section>
-      <div className="bg-green-600 py-4 lg:py-10 md:[height:80px]"></div>
+      <div className="py-4 lg:py-10 md:[height:80px]"></div>
+      <section>
+        <div>
+          <div className="w-full flex justify-center">
+            <button className="bg-green-600 animate-pulse p-2 px-8 rounded-md">
+              Book Ticket
+            </button>
+          </div>
+        </div>
+      </section>
+      {/* <section>
+        <div className="bg-green-600 py-4 flex justify-center">
+          <span className="p-4 font-semibold text-4xl font-sans text-white">Alumni Program</span>
+        </div>
+        <div className="w-full flex justify-center">
+          <Testimoni/>
+        </div>
+      </section> */}
+      <div className="py-4 lg:py-10 md:[height:80px]"></div>
+      {/* <section>
+      <div className="animate-pulse duration-800  bg-green-600 flex justify-center p-2 py-6">
+        <h className="text-2xl text-slate-200 font-bold font-sans">Sebarang pertanyaan sila hubungi En.Saiyyid <span className="text-black">01115403660</span></h>
+        </div>
+      </section> */}
+        
+      {/* <section>
+        <FormMmp/>
+      </section> */}
+      {/* <section>
+        <Moto/>
+      </section> */}
+      <div className="bg-white py-4 lg:py-10 md:[height:80px]"></div>
       <section>
       <div className="flex items-center">
         <Image
@@ -42,38 +90,14 @@ function page() {
           />
         </div>
       </section>
+      <div className="bg-white py-4 lg:py-10 md:[height:80px]"></div>
       <section>
-        <Countdown initialTargetDate={initialTargetDate}/>
+        <Faqticketmmp />
       </section>
       <section>
-        <div className="bg-green-600 py-4 flex justify-center">
-          <span className="p-4 font-semibold text-4xl font-sans text-white">Alumni Program</span>
-        </div>
-        <div className="w-full flex justify-center">
-          <Testimoni/>
-        </div>
-      </section>
-      <div className="py-4 lg:py-10 md:[height:80px]"></div>
-      <section>
-      <div className="animate-pulse duration-800  bg-green-600 flex justify-center p-2 py-6">
-        <h className="text-2xl text-slate-200 font-bold font-sans">Sebarang pertanyaan sila hubungi En.Saiyyid <span className="text-black">01115403660</span></h>
-        </div>
-      </section>
-        
-      <div className="py-4 lg:py-10 md:[height:80px]"></div>
-      <section>
-        <FormMmp/>
-      </section>
-      <section>
-        <Moto/>
-      </section>
-      <section>
-        <Footer/>
+        <Footer/>        
       </section>
       
-              <section>
-
-              </section>
     </main>
     </>  
   )
