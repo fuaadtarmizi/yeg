1c0dfcfd8e7a4f5dedd07fe30be2d83cb38c739b
fuaadtarmizi <fuaad2120@gmail.com>
2024-06-21 11:12:35 +0800

    test183

--- next.config.js
+++ next.config.js
@@ -1,17 +1,23 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
+// /**
+//  * @type {import('next').NextConfig}
+//  */
+// const nextConfig = {
 
-    images: {
-        loader: 'default',
-         unoptimized: true,
-      },
+//     images: {
+//         loader: 'default',
+//          unoptimized: true,
+//       },
 
-    output: 'export',
-  };
-  export default nextConfig;
-
-
+//     output: 'export',
+//   };
+//   export default nextConfig;
 
 
+
+
+// next.config.js
+
+export const experimental = {
+  // remove or comment out the invalid property
+  // images: {},
+};
