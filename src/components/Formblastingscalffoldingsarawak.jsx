049553cae39fafb471318d04b94b1f3b8f914b63
fuaadtarmizi <fuaad2120@gmail.com>
2024-06-25 16:18:46 +0800

    test194

--- src/components/Formblastingscalffoldingsarawak.jsx
+++ src/components/Formblastingscalffoldingsarawak.jsx
@@ -1,61 +1,44 @@
 "use client"
-import React, { useState, useEffect  } from 'react';
-
+import React, { useState } from 'react';
+import Image from 'next/image';
+import Footer from "@/components/Footer.jsx";
+import Navbar from "@/components/Navbar.jsx";
+import Sidebar from "@/components/Sidebar.jsx";
 
 export default function App() {
-  const [age, setAge] = useState('');
-
-    const [selectedItem, setSelectedItem] = useState(null);
-    const [selectedItem1, setSelectedItem1] = useState("Ads");
-
-    const [alertMessage, setAlertMessage] = useState('');
-    const [emailError, setEmailError] = useState('');
-
-
-    useEffect(() => {
-      if (selectedItem1 === 'Whatsup') {
-        Submit();
-      }
-    }, [selectedItem1]);
-
-
-    const handleChange = (e) => {
-      const value = e.target.value;
-      if (value === '' || (value >= 18 && value <= 35)) {
-        setAge(value);
-      }
-    };
-  
-
-    
-
+  const [alertMessage, setAlertMessage] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [formError, setFormError] = useState('');
 
   function Submit(e) {
+    e.preventDefault(); // Prevent default form submission behavior
+
     const emailValue = e.target.elements.Email.value;
+    const nameValue = e.target.elements.Name.value;
+    const phoneValue = e.target.elements.NumberPhone.value;
+    const ageValue = e.target.elements.Age.value;
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-
+    // Check if all fields are filled
+    if (!nameValue || !phoneValue || !emailValue || !ageValue) {
+      setFormError('All fields must be filled.');
+      return;
+    }
+    // Validate email format
     if (!emailRegex.test(emailValue)) {
       setEmailError('Email must be in a valid format.');
       alert('Email is incorrect!');
       return;
     }
-
     const formEle = document.querySelector("form");
-    const formDatab = new FormData(formEle);
-
-    formDatab.append("Program", selectedItem);
-    formDatab.append("Source", selectedItem1);
-
+    const formData = new FormData(formEle);
 
     fetch(
-
       "https://script.google.com/macros/s/AKfycbzjmTyHnFox-ovqkSpGEwLWddSilSjlLVs71zhpVYIQQ6UAVXHNTWxqYARGPw1KQmAi/exec",
       {
         method: "POST",
-        body: formDatab
+        body: formData
       }
-      
     )
       .then((res) => res.json())
       .then((data) => {
@@ -65,104 +48,66 @@ export default function App() {
         console.log(error);
       });
 
-      setAlertMessage('Form submitted successfully!');
-      alert('Penghantaran Berjaya!, Pegawai kami akan menghubungi anda secepat mungkin');
+    setAlertMessage('Form submitted successfully!');
+    alert('Penghantaran Berjaya!, Pegawai kami akan menghubungi anda secepat mungkin');
+    setFormError(''); // Clear form error message
+    setEmailError(''); // Clear email error message
   }
-    
-
 
   return (
     <main>
-    <div className="bg-white">
-    <div className=" hidden lg:block"></div>
-        <div className="p-4  lg:flex justify-center  ">     
-      <div className="lg:w-1/3 ">
-      <form className=" backdrop-blur-lg w-full p-6 border space-y-5 rounded-lg shadow-lg" onSubmit={(e) => Submit(e)}>
-      <div className="py-6 text-black text-3xl font-bold">
-             <h>Borang Pertanyaan</h>
-         </div>
-            <div className=" grid space-y-4">
-      <div className="grid rounded-md space-y-1 ">
-        <h className="">Nama</h>
-      <input className="bg-gray-200 px-3 py-2 hover:shadow-md duration-500 rounded-md " placeholder="" name="Name" type="text" />
-      </div>
-          <div className="grid space-y-1">
-          <h>Nombor Telefon </h>
-          <input className="bg-gray-200 px-3 py-2 hover:shadow-md duration-500 rounded-md" placeholder="" name="NumberPhone" type="text" />
+      <div className="bg-white">
+        <div className="hidden lg:block">j</div>
+        <div className="p-4 lg:flex justify-center">
+          <div className="lg:w-1/3">
+            <form className="backdrop-blur-lg w-full p-6 border space-y-5 rounded-lg shadow-lg" onSubmit={(e) => Submit(e)}>
+              <div className="py-6 text-black text-3xl font-bold">
+                <h>Borang Pertanyaan</h>
+              </div>
+              <div className="grid space-y-4">
+                <div className="grid rounded-md space-y-1">
+                  <h className="">Nama</h>
+                  <input className="bg-gray-200 px-3 py-2 hover:shadow-md duration-500 rounded-md" placeholder="" name="Name" type="text" />
+                </div>
+                <div className="grid space-y-1">
+                  <h>Nombor Telefon</h>
+                  <input className="bg-gray-200 px-3 py-2 hover:shadow-md duration-500 rounded-md" placeholder="" name="NumberPhone" type="text" />
+                </div>
+              </div>
+              <div className="grid space-y-4">
+                <div className="grid rounded-md space-y-1">
+                  <h>Email</h>
+                  <input className="bg-gray-200 px-3 py-2 hover:shadow-md duration-500 rounded-md" placeholder="" name="Email" type="text" />
+                </div>
+                <div className="grid space-y-1">
+                  <h>Umur</h>
+                  <input className="bg-gray-200 px-3 py-2 hover:shadow-md duration-500 rounded-md" placeholder="" name="Age" type="text" />
+                </div>
+              </div>
+
+              <div className="pt-4">
+                <button className="px-6 py-2 border rounded-md bg-yellow-500 hover:bg-yellow-600 hover:shadow-md duration-300" name="Submit" type="submit">Submit</button>
+              </div>
+            </form>
+            {formError && (
+              <div className="alert">
+                {formError}
+              </div>
+            )}
+            {emailError && (
+              <div className="alert">
+                {emailError}
+              </div>
+            )}
+            {alertMessage && (
+              <div className="alert">
+                {alertMessage}
+              </div>
+            )}
           </div>
-          </div>
-
-          <div className="grid space-y-4">
-
-      <div className="grid rounded-md space-y-1 ">
-        <h>Email</h>
-      <input className="bg-gray-200 px-3 py-2 hover:shadow-md duration-500 rounded-md " 
-        placeholder="" 
-        name="Email" 
-        type="text" />
-      </div>
-
-          <div className="grid space-y-1">
-          <h>Umur</h>
-          <input
-                    className="bg-gray-200 px-3 py-2 hover:shadow-md duration-500 rounded-md"
-                    placeholder=""
-                    name="Age"
-                    type="number"
-                    value={age}
-                    onChange={handleChange}
-                    min="18"
-                    max="35"
-                  />
-          </div>
-          </div>
-
-         
-          {/* <div className="grid space-y-1">
-          <h>Nama Penjaga</h>
-          <input className="bg-gray-200 px-3 py-2 hover:shadow-md duration-500 rounded-md" placeholder="" name="ParentName" type="text" />
-          </div>
-
-          <div className="grid space-y-1">
-          <h>Nombor Penjaga</h>
-          <input className="bg-gray-200 px-3 py-2 hover:shadow-md duration-500 rounded-md" placeholder="" name="ParentNumber"  type="text" />
-          </div> */}
-
-          
-    <div className="pt-4">
-    <button className="px-6 py-2 border rounded-md bg-yellow-500 hover:bg-yellow-600 hover:shadow-md duration-300" name="Submit" type="submit" >Submit</button>
-    </div>
-        </form>
-        {emailError && (
-        <div className="alert">
-          {emailError}
         </div>
-      )}
-      {alertMessage && (
-        <div className="alert">
-          {alertMessage}
-        </div>
-        )}
-    </div>
-    </div> 
-    </div>
-    
-   
+      </div>
     </main>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
