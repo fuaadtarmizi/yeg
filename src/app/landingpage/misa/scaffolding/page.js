bab675bacc369c795fb9c0c2528c411371740eed
fuaadtarmizi <fuaad2120@gmail.com>
2024-08-01 18:39:39 +0800

    test208

--- src/app/landingpage/misa/scaffolding/page.js
+++ src/app/landingpage/misa/scaffolding/page.js
@@ -196,15 +196,18 @@ function page() {
     <section>
       <div className="flex justify-center py-6">
       <button className=" z-0 bg-orange-600 rounded-full transition duration-150 ease-out hover:bg-orange-400">
-      <a href="/" className="flex pl-5 p-3">
+      <a href="#target-section" 
+        to="target-section"
+        smooth={true}
+        duration={500} 
+        className="flex pl-5 p-3 cursor-pointer">
         <span className="text-white text-xl">Dapatkan maklumat lebih lanjut</span>
         <svg
           width="20"
           height="28"
           viewBox="0 0 15 15"
           fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
+          xmlns="http://www.w3.org/2000/svg">
         <path
             d="M6.18194 4.18185C6.35767 4.00611 6.6426 4.00611 6.81833 4.18185L9.81833 7.18185C9.90272 7.26624 9.95013 7.3807 9.95013 7.50005C9.95013 7.6194 9.90272 7.73386 9.81833 7.81825L6.81833 10.8182C6.6426 10.994 6.35767 10.994 6.18194 10.8182C6.0062 10.6425 6.0062 10.3576 6.18194 10.1819L8.86374 7.50005L6.18194 4.81825C6.0062 4.64251 6.0062 4.35759 6.18194 4.18185Z"
             fill="currentColor"
@@ -244,7 +247,7 @@ function page() {
         </div>
     </section>
     <div className="py-2 md:py-4 lg:py-8"></div>
-    <section>
+    <section id="target-section">
       <div className="w-full bg-slate-300 p-4 rounded-sm">
         <div className=" grid justify-center">
           <div className="p-2">
