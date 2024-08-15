bbbe7499ecf40c4dc9209ae785f8dc43f0199038
fuaadtarmizi <fuaad2120@gmail.com>
2024-08-15 17:05:36 +0800

    test228

--- src/components/lpdegreehajumrah/SliderKonvo.jsx
+++ src/components/lpdegreehajumrah/SliderKonvo.jsx
@@ -32,7 +32,7 @@ export default function App() {
           <div className="w-full flex justify-center">
             <Image 
             className="w-full rounded-md"
-            src="/konvo1.jpg"
+            src="/konvoo1.jpg"
             alt="konvo1"
             width="1000"
             height="50"
@@ -46,7 +46,7 @@ export default function App() {
           <div className="w-full flex justify-center">
             <Image 
             className="w-full rounded-md"
-            src="/konvo2.jpg"
+            src="/konvoo2.jpg"
             alt="konvo2"
             width="1000"
             height="50"
@@ -60,7 +60,7 @@ export default function App() {
           <div className="w-full flex justify-center">
             <Image 
             className="w-full rounded-md"
-            src="/konvo3.jpg"
+            src="/konvoo3.jpg"
             alt="konvo3"
             width="1000"
             height="50"
@@ -74,7 +74,7 @@ export default function App() {
           <div className="w-full flex justify-center">
             <Image 
             className="w-full rounded-md"
-            src="/konvo4.jpg"
+            src="/konvoo4.jpg"
             alt="konvo4"
             width="1000"
             height="50"
@@ -88,7 +88,7 @@ export default function App() {
           <div className="w-full flex justify-center">
             <Image 
             className="w-full rounded-md"
-            src="/konvo5.jpg"
+            src="/konvoo5.jpg"
             alt="konvo5"
             width="1000"
             height="50"
@@ -102,7 +102,7 @@ export default function App() {
           <div className="w-full flex justify-center">
             <Image 
             className="w-full rounded-md"
-            src="/konvo6.jpg"
+            src="/konvoo6.jpg"
             alt="konvo6"
             width="1000"
             height="50"
