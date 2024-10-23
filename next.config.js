637d955c804f98aad02138d5478b191e1b6389b1
fuaadtarmizi <fuaad2120@gmail.com>
2024-10-23 17:04:44 +0800

    test263

--- next.config.js
+++ next.config.js
@@ -1,11 +1,24 @@
+// module.exports = {
+//   swcMinify: false,
+//   experimental: {
+//     // Remove or comment out the invalid property if present
+//     // images: {}, 
+//   },
+//   output: 'export',
+//   images: { 
+//     unoptimized: true,
+//   },
+// };
+
+// next.config.js
+
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+
+
 module.exports = {
+  output: 'export', // This is key for static export
   swcMinify: false,
-  experimental: {
-    // Remove or comment out the invalid property if present
-    // images: {}, 
-  },
-  output: 'export',
-  images: { 
-    unoptimized: true,
+  images: {
+    unoptimized: true, // Required for static exports
   },
 };
