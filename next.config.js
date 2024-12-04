39d61f8d7b2e8fed14cb1a8d0dd1cc57212992ac
fuaadtarmizi <fuaad2120@gmail.com>
2024-12-04 11:37:17 +0800

    test280

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
-// module.exports = {
-//   swcMinify: false,
-//   experimental: {
-//     // Remove or comment out the invalid property if present
-//     // images: {}, 
-//   },
-//   output: 'export',
-//   images: { 
-//     unoptimized: true, 
-//   },
-// };
+module.exports = {
+  swcMinify: false,
+  experimental: {
+    // Remove or comment out the invalid property if present
+    // images: {}, 
+  },
+  output: 'export',
+  images: { 
+    unoptimized: true, 
+  },
+};
 
-// next.config.js
+next.config.js
 
 
 
@@ -23,10 +23,10 @@
 // };
 
 
-/** @type {import('next').NextConfig} */
-module.exports = {
-  output: 'export', // Enables static export
-  images: {
-    unoptimized: true, // Required for static exports
-  },
-};
+// /** @type {import('next').NextConfig} */
+// module.exports = {
+//   output: 'export', // Enables static export
+//   images: {
+//     unoptimized: true, // Required for static exports
+//   },
+// };
