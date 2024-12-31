ebfcd46d8efe2db29c9eb864df68ac215d29aa2e
fuaadtarmizi <fuaad2120@gmail.com>
2024-12-31 16:07:14 +0800

    test344

--- next.config.js
+++ next.config.js
@@ -1,14 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// module.exports = {
-//   swcMinify: false, // Disables SWC minification
-//   output: 'export', // Enables static export
-//   images: {
-//     unoptimized: true, // Required for static exports
-//   },
-// };
-
-
-
 /** @type {import('next').NextConfig} */
 module.exports = {
   swcMinify: false, // Disable SWC minification (optional)
--- src/components/PopupAds.jsx
+++ src/components/PopupAds.jsx
@@ -1,4 +1,7 @@
+'use client'
 import React, { useState, useEffect } from "react";
+import Image from 'next/image'
+
 
 const PopupAd = () => {
   const [showPopup, setShowPopup] = useState(false);
@@ -28,19 +31,22 @@ const PopupAd = () => {
 
   return (
     showPopup && (
-      <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50">
-        <div className="bg-white p-6 rounded shadow-lg relative">
+      <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-opacity-50 z-50">
+        <div className="rounded shadow-lg relative">
           <button
             className="absolute top-2 right-2 text-gray-600"
             onClick={handleClose}
-          >
-            ✕
+          > ✕
           </button>
-          <h2 className="text-lg font-bold mb-4">Exciting News!</h2>
-          <p>
-            We're working on a brand-new website design to serve you better.
-            Stay tuned for an upgraded experience!
-          </p>
+          <Image 
+          className="rounded"
+          src="/newyear.jpg"
+          alt="newyear"
+          width="1200"
+          height="80"
+          style={{
+            objectFit:"cover",
+          }}/>
         </div>
       </div>
     )
