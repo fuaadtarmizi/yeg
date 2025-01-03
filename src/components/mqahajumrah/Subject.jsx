3b36dccc36d34e9212887fce3138263b45703387
fuaadtarmizi <fuaad2120@gmail.com>
2025-01-03 17:22:38 +0800

    test348

---
+++ src/components/mqahajumrah/Subject.jsx
@@ -0,0 +1,186 @@
+import React from 'react'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
+function Syarat() {
+  return (
+    <>
+    <div className="p-2  ">
+    <Accordion type="single" collapsible className="w-full">
+      <AccordionItem value="item-1">
+        <AccordionTrigger> Program Diploma Haji Umrah</AccordionTrigger>
+        <AccordionContent>
+          <div className="space-y-2">
+            <div className="bg-green-200">
+              <p className="font-bolt text-lg">Semester 1</p>
+              <li className="">Fiqh Haji & Umrah I (Ibadah Haji & Umrah)</li>
+              <li className="">Introduction to Business</li>
+              <li className="">Islamic Management</li>
+              <li className="">Introduction to tourism & Hospitality</li>
+              <li className="">Penghayatan Etika & Peradaban Bahasa Melayu komunikasi</li>
+              <li className="">English Proficiency</li>
+              <li className="">Travel Geography</li>
+              </div>
+              <div className="bg-green-100">
+              <p className="font-bolt text-lg">Semester 2</p>
+              <li className="">Fiqh Haji & Umrah II (Ibadah Haji & Umrah)</li>
+              <li className="">Transportation Operation in Tourism</li>
+              <li className="">Business Comunication</li>
+              <li className="">Computer Applications</li>
+              <li className="">Thirds Language (Arabic for Comunication I)</li>
+              <li className="">Bahasa Kebangsaan A / Business Etique</li>
+              </div>
+              <div className="bg-green-200">
+              <p className="font-bolt text-lg">Semester 3</p>
+              <li className="">Fiqh Haji & Umrah III (Ibadah Haji & Umrah)</li>
+              <li className="">Islamic Theology</li>
+              <li className="">Third Language (Arabic for Comunication)</li>
+              </div>
+              <div className="bg-green-100">
+              <p className="font-bolt text-lg">Semester 4</p>
+              <li className="">Travel & Tour Agency Operation</li>
+              <li className="">Profesional Ethics in Malaysia</li>
+              <li className="">Airpot Handling & Procedures</li>
+              <li className="">Human Resources Management</li>
+              <li className="">Principle of Marketing</li>
+              </div>
+              <div className="bg-green-200">
+              <p className="font-bolt text-lg">Semester 5</p>
+              <li className="">Principle Operation Management</li>
+              <li className="">Tourism Cross Culture and Heritage</li>
+              <li className="">Outbound Tour</li>
+              <li className="">E-commerce</li>
+              <li className="">Community Service / Event Management</li>
+              </div>
+              <div className="bg-green-100">
+              <p className="font-bolt text-lg">Semester 6</p>
+              <li className="">Organization ehavior</li>
+              <li className="">Tourism & Hospitality Law for Haji & Umrah</li>
+              <li className="">Islamic Entreprenuership</li>
+              <li className="">Strategic Management</li>
+              </div>
+              <div className="bg-green-100">
+              <p className="font-bolt text-lg">Semester 7</p>
+              <li className="">Industri Traning</li>
+              </div>
+          </div>
+        </AccordionContent>
+      </AccordionItem>
+      
+    
+          
+      
+    
+
+
+      <AccordionItem value="item-2">
+        <AccordionTrigger>Pra-Diploma</AccordionTrigger>
+        <AccordionContent>
+        <div className="bg-green-100">
+          <p className="font-bolt text-lg">1st year</p>
+          <li className="">Business English</li>
+          <li className="">Principle of Management</li>
+          <li className="">Introduction of Information Technology</li>
+          <li className="">Micoeconomics</li>
+          <li className="">Malaysian Studies / Bahasa Melayu Komunikasi 1</li>
+          <li className="">Principles of Marketing</li>
+          <li className="">Business Comunication</li>
+          <li className="">Professional Ethics in Malaysia</li>
+          <li className="">Human Resources Management</li>
+          <li className="">Micoeconomics</li>
+          <li className="">Basic Business Mathematics</li>
+          <li className="">Principle of Accounting</li>
+          <li className="">Operation Management</li>
+          <li className="">Financial Management</li>
+          <li className="">Business law</li>
+          <li className="">Business Etiquette</li>
+          <li className="">Business Services</li>
+          </div>
+          <div className="bg-green-100">
+          <p className="font-bolt text-lg">2nd year</p>
+          <li className="">Business Statics</li>
+          <li className="">E-Commerce</li>
+          <li className="">Organization Behaviour</li>
+          <li className="">Enterprenuership</li>
+          </div>          
+        </AccordionContent>
+      </AccordionItem>
+
+
+      <AccordionItem value="item-1">
+        <AccordionTrigger> Program Diploma Haji Umrah - WBL</AccordionTrigger>
+        <AccordionContent>
+          <div className="space-y-2">
+            <div className="bg-green-200">
+              <p className="font-bolt text-lg">Semester 1</p>
+              <li className="">Fiqh Haji & Umrah I (Ibadah Haji & Umrah)</li>
+              <li className="">Introduction to Business</li>
+              <li className="">Islamic Management</li>
+              <li className="">Introduction to tourism & Hospitality</li>
+              <li className="">Penghayatan Etika & Peradaban Bahasa Melayu komunikasi</li>
+              <li className="">English Proficiency</li>
+              <li className="">Travel Geography</li>
+              </div>
+              <div className="bg-green-100">
+              <p className="font-bolt text-lg">Semester 2</p>
+              <li className="">Fiqh Haji & Umrah II (Ibadah Haji & Umrah)</li>
+              <li className="">Transportation Operation in Tourism</li>
+              <li className="">Business Comunication</li>
+              <li className="">Computer Applications</li>
+              <li className="">Thirds Language (Arabic for Comunication I)</li>
+              <li className="">Bahasa Kebangsaan A / Business Etique</li>
+              </div>
+              <div className="bg-green-200">
+              <p className="font-bolt text-lg">Semester 3</p>
+              <li className="">Fiqh Haji & Umrah III (Ibadah Haji & Umrah)</li>
+              <li className="">Islamic Theology</li>
+              <li className="">Third Language (Arabic for Comunication)</li>
+              </div>
+              <div className="bg-green-100">
+              <p className="font-bolt text-lg">Semester 4</p>
+              <li className="">Travel & Tour Agency Operation</li>
+              <li className="">Profesional Ethics in Malaysia</li>
+              <li className="">Airpot Handling & Procedures</li>
+              <li className="">Human Resources Management</li>
+              <li className="">Principle of Marketing</li>
+              </div>
+              <div className="bg-green-200">
+              <p className="font-bolt text-lg">Semester 5</p>
+              <li className="">Principle Operation Management</li>
+              <li className="">Tourism Cross Culture and Heritage</li>
+              <li className="">Outbound Tour</li>
+              <li className="">E-commerce</li>
+              <li className="">Community Service / Event Management</li>
+              </div>
+              <div className="bg-green-100">
+              <p className="font-bolt text-lg">Semester 6</p>
+              <li className="">Organization ehavior</li>
+              <li className="">Tourism & Hospitality Law for Haji & Umrah</li>
+              <li className="">Islamic Entreprenuership</li>
+              <li className="">Strategic Management</li>
+              </div>
+              <div className="bg-green-100">
+              <p className="font-bolt text-lg">Semester 7</p>
+              <li className="">Industri Traning</li>
+              </div>
+          </div>
+        </AccordionContent>
+      </AccordionItem>
+
+
+      
+    </Accordion>
+    </div>
+    </>
+  )
+}
+
+export default Syarat
+
+
+
+
