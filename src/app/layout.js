54d6852244f38322e0a1f1dddbf6e9b782ce9e41
fuaadtarmizi <fuaad2120@gmail.com>
2025-01-14 17:26:46 +0800

    test369

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-      <head>
+      <Head>
           <meta charset="UTF-8"/>
           <title>{metadata.title}</title>
           <meta name="description" content={metadata.description} />
@@ -35,6 +35,25 @@ export default function RootLayout({ children }) {
           <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
           <meta name="google-site-verification" content="IpMLN0O17LzsxofVFp5HDnRje-CPwKC5j1QK05iBcdM" />
           <link rel="icon" href="./yeglogowebsite.svg" sizes="20x20" type="image/svg+xml" />
+
+          {/* tiktok pixel */}
+          <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function (w, d, t) {
+                w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(
+                var e=ttq._i[t]=[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script")
+                ;n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
+
+                ttq.load('CU32N3RC77U5K61FFQIG');
+                ttq.page();
+              }(window, document, 'ttq');
+            `,
+          }}
+        />
+
+
+
 
           {/* gtag.js */}
           <Script 
@@ -120,7 +139,7 @@ export default function RootLayout({ children }) {
             }),
           }}
         />
-      </head>
+      </Head>
       <body className={inter.className}>
       <Header/>
       <main>{children}</main>
