7728543c17f9745ab27c3cc7a338bd94b182ab29
fuaadtarmizi <fuaad2120@gmail.com>
2025-01-14 11:12:15 +0800

    test367

--- src/components/mqahajumrah/Testimoni.jsx
+++ src/components/mqahajumrah/Testimoni.jsx
@@ -8,63 +8,57 @@ function App () {
   return (
     <main>
         <section11>
-            <div className=" bg-green-800 w-full h-full px-4 lg:px-28">
+            <div className=" bg-green-800 w-full h-full px-4 lg:px-2">
             <div className="py-4"><h className="text-4xl font-extrabold text-white">Testimoni Dhumy</h></div>
-            <div className="h-full grid grid-cols-2  lg:py-4 md:grid-cols-4 gap-4 items-center">
-            <a href="https://www.tiktok.com/@pengurusanhajiumrah/video/7454090052411477256?is_from_webapp=1&sender_device=pc"
-            className="flex hover:scale-150 duration-300">
-            <video autoplay="autoplay" loop="loop" muted="muted"
-                className="rounded-md top-0 left-0 w-full h-full object-cover" 
-                width="390" 
-                height="890">
-                <source src="/testi1.mp4" type="video/mp4"/>
-            </video>
-            </a>
-            <a href="https://www.tiktok.com/@pengurusanhajiumrah/video/7453861488747449608?is_from_webapp=1&sender_device=pc"
-            className="flex hover:scale-150 duration-300">
-            <video autoplay="autoplay" loop="loop" muted="muted"
-                className="rounded-md top-0 left-0 w-full h-full object-cover" 
-                width="390" 
-                height="890">
-                <source src="/testi2.mp4" type="video/mp4"/>
-            </video>
-            </a>
-            <a href="https://www.tiktok.com/@pengurusanhajiumrah/video/7451224071339052296?is_from_webapp=1&sender_device=pc"
-            className="flex hover:scale-150 duration-300">
-            <video autoplay="autoplay" loop="loop" muted="muted"
-                className="rounded-md top-0 left-0 w-full h-full object-cover" 
-                width="390" 
-                height="890">
-                <source src="/testi3.mov" type="video/mp4"/>
-            </video>
-            </a>
-            <a href="https://www.tiktok.com/@pengurusanhajiumrah/video/7452697755891338504?is_from_webapp=1&sender_device=pc"
-            className="flex hover:scale-150 duration-300">
-            <video autoplay="autoplay" loop="loop" muted="muted"
-                className="rounded-md top-0 left-0 w-full h-full object-cover" 
-                width="390" 
-                height="890">
-                <source src="/testi4.mp4" type="video/mp4"/>
-            </video>
-            </a>
-            {/* <a href="https://www.tiktok.com/@pengurusanhajiumrah/video/7454958664617692424?is_from_webapp=1&sender_device=pc"
-            className="flex hover:scale-150 duration-300">
-            <video autoplay="autoplay" loop="loop" muted="muted"
-                className="rounded-md top-0 left-0 w-full h-full object-cover" 
-                width="390" 
-                height="890">
-                <source src="/testi5.mp4" type="video/mp4"/>
-            </video>
-            </a>
-            <a href="https://www.tiktok.com/@pengurusanhajiumrah/video/7451930447564541202?is_from_webapp=1&sender_device=pc"
-            className="flex hover:scale-150 duration-300">
-            <video autoplay="autoplay" loop="loop" muted="muted"
-                className="rounded-md top-0 left-0 w-full h-full object-cover" 
-                width="390" 
-                height="890">
-                <source src="/testi6.mp4" type="video/mp4"/>
-            </video>
-            </a> */}
+            <div className="h-full grid grid-cols-1 lg:py-6 md:grid-cols-6 gap-4 items-center">
+            <iframe
+            src="https://www.tiktok.com/embed/7454090052411477256"
+            width="200"
+            height="550"
+            allow="autoplay"
+            frameborder="0"
+            className="rounded-md w-full border"
+            ></iframe>
+            <iframe
+            src="https://www.tiktok.com/embed/7453861488747449608"
+            width="180"
+            height="550"
+            allow="autoplay"
+            frameborder="0"
+            className="rounded-md w-full border"
+            ></iframe>
+            <iframe
+            src="https://www.tiktok.com/embed/7451224071339052296"
+            className="rounded-md w-full border"
+            allow="autoplay"
+            width="180"
+            height="550"
+            frameborder="0"
+            ></iframe>
+            <iframe
+            src="https://www.tiktok.com/embed/7452697755891338504"
+            className="rounded-md w-full border"
+            allow="autoplay"
+            width="180"
+            height="550"
+            frameborder="0"
+            ></iframe>
+            <iframe
+            src="https://www.tiktok.com/embed/7454958664617692424"
+            className="rounded-md w-full border"
+            allow="autoplay"
+            width="180"
+            height="550"
+            frameborder="0"
+            ></iframe>
+            <iframe
+            src="https://www.tiktok.com/embed/7451930447564541202"
+            className="rounded-md w-full border"
+            allow="autoplay"
+            width="180"
+            height="550"
+            frameborder="0"
+            ></iframe>
             </div>
             </div>
         </section11>
