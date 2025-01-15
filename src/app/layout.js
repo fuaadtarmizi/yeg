6c3016b6eae6a2ebaa2bd344c163a11b6e1a655a
fuaadtarmizi <fuaad2120@gmail.com>
2025-01-15 18:33:16 +0800

    test374

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import './globals.css'
 import { Analytics } from '@vercel/analytics/react';
 import { Inter } from 'next/font/google'
@@ -18,42 +18,6 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
 
-  useEffect(() => {
-    {/* tiktok pixel */}
-    (function (w, d, t) {
-      w.TiktokAnalyticsObject = t;
-      var ttq = w[t] = w[t] || [];
-      ttq.methods = ["page", "track", "identify", "instances", "debug", "on", "off", "once", "ready", "alias", "group", "enableCookie", "disableCookie", "holdConsent", "revokeConsent", "grantConsent"];
-      ttq.setAndDefer = function (t, e) {
-        t[e] = function () {
-          t.push([e].concat(Array.prototype.slice.call(arguments, 0)));
-        };
-      };
-      for (var i = 0; i < ttq.methods.length; i++) {
-        ttq.setAndDefer(ttq, ttq.methods[i]);
-      }
-      ttq.load = function (e, n) {
-        var r = "https://analytics.tiktok.com/i18n/pixel/events.js";
-        var o = n && n.partner;
-        ttq._i = ttq._i || {};
-        ttq._i[e] = [];
-        ttq._i[e]._u = r;
-        ttq._t = ttq._t || {};
-        ttq._t[e] = +new Date();
-        ttq._o = ttq._o || {};
-        ttq._o[e] = n || {};
-        n = document.createElement("script");
-        n.type = "text/javascript";
-        n.async = !0;
-        n.src = r + "?sdkid=" + e + "&lib=" + t;
-        e = document.getElementsByTagName("script")[0];
-        e.parentNode.insertBefore(n, e);
-      };
-      ttq.load("CU32N3RC77U5K61FFQIG");
-      ttq.page();
-    })(window, document, "ttq");
-  }, []);   
-
   return (
     <html lang="en">
       <Head>
@@ -71,6 +35,43 @@ export default function RootLayout({ children }) {
           <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
           <meta name="google-site-verification" content="IpMLN0O17LzsxofVFp5HDnRje-CPwKC5j1QK05iBcdM" />
           <link rel="icon" href="./yeglogowebsite.svg" sizes="20x20" type="image/svg+xml" />
+
+          {/* TikTok Pixel */}
+          <Script id="tiktok-pixel" strategy="afterInteractive">
+          {`
+            !function (w, d, t) {
+              w.TiktokAnalyticsObject = t;
+              var ttq = w[t] = w[t] || [];
+              ttq.methods = ["page", "track", "identify", "instances", "debug", "on", "off", "once", "ready", "alias", "group", "enableCookie", "disableCookie"];
+              ttq.setAndDefer = function (t, e) {
+                t[e] = function () {
+                  t.push([e].concat(Array.prototype.slice.call(arguments, 0)));
+                };
+              };
+              for (var i = 0; i < ttq.methods.length; i++) {
+                ttq.setAndDefer(ttq, ttq.methods[i]);
+              }
+              ttq.load = function (e, n) {
+                var r = "https://analytics.tiktok.com/i18n/pixel/events.js";
+                ttq._i = ttq._i || {};
+                ttq._i[e] = [];
+                ttq._i[e]._u = r;
+                ttq._t = ttq._t || {};
+                ttq._t[e] = +new Date();
+                ttq._o = ttq._o || {};
+                ttq._o[e] = n || {};
+                n = document.createElement("script");
+                n.type = "text/javascript";
+                n.async = !0;
+                n.src = r + "?sdkid=" + e + "&lib=" + t;
+                e = document.getElementsByTagName("script")[0];
+                e.parentNode.insertBefore(n, e);
+              };
+              ttq.load('CU32N3RC77U5K61FFQIG');
+              ttq.page();
+            }(window, document, 'ttq');
+          `}
+        </Script>
 
           
           
