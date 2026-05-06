d12f495c417b8caa981260df5e6323213739ca93
dinAliah <ndina1943@gmail.com>
2026-05-06 11:42:27 +0800

    Update MPHU

---
+++ src/components/MarqueenBanner.jsx
@@ -0,0 +1,14 @@
+export default function MarqueeBanner() {
+  return (
+    <div style={{ background: '#2d7a2d', overflow: 'hidden', padding: '10px 0' }}>
+      <div className="marquee-track">
+        <span style={{ fontSize: '15px', fontWeight: 500, color: '#fff', whiteSpace: 'nowrap', letterSpacing: '0.03em' }}>
+          | MPHU 2026 | MPHU 2026 | MPHU 2026 | MPHU 2026 | MPHU 2026 | MPHU 2026 | MPHU 2026 | MPHU 2026 &nbsp;&nbsp;&nbsp;
+        </span>
+        <span style={{ fontSize: '15px', fontWeight: 500, color: '#fff', whiteSpace: 'nowrap', letterSpacing: '0.03em' }}>
+          | MPHU 2026 | MPHU 2026 | MPHU 2026 | MPHU 2026 | MPHU 2026 | MPHU 2026 | MPHU 2026 | MPHU 2026 &nbsp;&nbsp;&nbsp;
+        </span>
+      </div>
+    </div>
+  );
+}
