c2ba1d528375f3208c54dca0d8a4ca86567f41be
dinAliah <ndina1943@gmail.com>
2026-05-08 18:05:24 +0800

    MPHU

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,7 +42,7 @@ function Navbar() {
                   <a className="text-md font-light ml-2">Fakulti Pengurusan Haji & Umrah (FPHU)</a></div>
                   </MenubarSubTrigger>
                   <MenubarSubContent>
-                    <MenubarItem><a className="text-md font-light ml-2" href="/diplomahajiumrah1">Diploma + Degree Hajj & Umrah Management</a></MenubarItem>
+                    <MenubarItem><a className="text-md font-light ml-2" href="/MPHU">Diploma + Degree Hajj & Umrah Management</a></MenubarItem>
                     <MenubarItem><a className="text-md font-light ml-2" href="https://e-dhumy.com/masterhajiumrah">Master Hajj & Umrah Management</a></MenubarItem>
                     <MenubarItem><a className="text-md font-light ml-2" href="/kursuskerjaya/sphu/shum">Profesional Hajj & Umrah Certificate</a></MenubarItem>
                     <MenubarItem><a className="text-md font-light ml-2" href="https://e-dhumy.com/phdhajjumrah">PhD Hajj & Umrah Management</a></MenubarItem>
