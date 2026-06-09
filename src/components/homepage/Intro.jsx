ca46e66b2ddd55a7f085a00247aec1107d96f445
Dina <ndina1943@gmail.com>
2026-06-09 15:37:14 +0800

    first commit

--- src/components/homepage/Intro.jsx
+++ src/components/homepage/Intro.jsx
@@ -2,18 +2,20 @@ import React from 'react'
 import Footer from "@/components/Footer.jsx"
 import Navbar from "@/components/Navbar.jsx"
 import Sidebar from "@/components/Sidebar.jsx"
+
+
 function Intro() {
   return (
     <>
     <Sidebar/>
     <Navbar className="bg-gradient-to-r from-[#151c41] to-[rgba(11,24,54,0)"/>
-    <section1 className="relative">
+    <section className="relative">
     <div className="absolute top-0 left-0 h-full w-1/4 "></div>
     <div className="bg-black max-w-full lg:px-3 flex justify-center ">
         <video
-         controls autoPlay muted playsInline className="w-full h-full"><source src="/yegintro.mp4" type="video/mp4" /></video>
+         src ="/yegintro.mp4" controls autoPlay muted playsInline className="w-full h-full object-cover" />
       </div>
-        </section1>
+        </section>
         </>
     
   )
