97554516992b387388a7cbbe216269f699289591
Dina <ndina1943@gmail.com>
2026-06-09 11:22:07 +0800

    first commit

--- next.config.js
+++ next.config.js
@@ -2,8 +2,7 @@
 module.exports = {
   swcMinify: false, // Disable SWC minification (optional)
   output: 'export', // Enable static export mode
-  basePath: '/yeg',
-  assetPrefix: '/yeg/',
+
 
   images: {
     unoptimized: true, // Required for GitHub Pages
