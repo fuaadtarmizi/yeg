17967afe138cb246d1ee24ff4539ec4a594333fa
Dina <ndina1943@gmail.com>
2026-06-09 10:11:30 +0800

    first commit

--- src/components/homepage/Intro.jsx
+++ src/components/homepage/Intro.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import Navbar25 from '@/components/homepage/items/Navbar25'
-import Sidebar from '@/components/homepage/items/Sidebar'
-
+import Footer from "@/components/Footer.jsx"
+import Navbar from "@/components/Navbar.jsx"
+import Sidebar from "@/components/Sidebar.jsx"
 function Intro() {
   return (
     <>
-    {/* <Sidebar/>
-    <Navbar25/ className="bg-gradient-to-r from-[#151c41] to-[rgba(11,24,54,0)"> */}
+    <Sidebar/>
+    <Navbar className="bg-gradient-to-r from-[#151c41] to-[rgba(11,24,54,0)"/>
     <section1 className="relative">
     <div className="absolute top-0 left-0 h-full w-1/4 "></div>
     <div className="bg-black max-w-full lg:px-3 flex justify-center ">
-      <video className="w-[1400px]"controls playsInline autoPlay><source src="yegintro.mp4" type="video/mp4" /></video>
+      <video className="w-[1400px]"controls playsInline autoPlay><source src="C:/Users/USER/yeg/public/yegintro.mp4" type="video/mp4" /></video>
       </div>
         </section1>
         </>
