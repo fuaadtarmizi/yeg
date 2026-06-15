718588aebbb929a450764c19624bf6458825422d
Dina <ndina1943@gmail.com>
2026-06-15 14:56:51 +0800

    Video fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   swcMinify: false, // Disable SWC minification (optional)
-  output: 'export', // Enable static export mode
+  
   
   images: {
     unoptimized: true, // Required for GitHub Pages
