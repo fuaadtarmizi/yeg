b68158fe19aee1e1cf71d36c6e0bbf511d686391
Dina <ndina1943@gmail.com>
2026-06-15 11:40:57 +0800

    Video

--- src/components/homepage/Entertainment.jsx
+++ src/components/homepage/Entertainment.jsx
@@ -1,27 +1,26 @@
 import React from 'react'
-import Image from "next/image"
-import ReactPlayer from 'react-player'
 
 
 
 function Intro() {
   return (
     <>
-    <section11>
+    <section>
       <div className=" bg-slate-800 w-full h-full px-4 lg:px-28"> 
         <div className="bg-yellow-600 w-1/2 text-transparent rounded-r-md">a</div>
         <div className="w-full h-full grid grid-cols-1 lg:grid-cols-3 lg:py-6">
         <div className="grid py-3 ">
-      <h className="text-base font-semibold text-yellow-800 ">OUR ENTERTAINMENT:</h>
-      <h className="text-4xl font-extrabold text-white ">EXPLORE OUR PROGRAMME ON MEDIA SOCIAL</h>
+      <h2 className="text-base font-semibold text-yellow-800 ">OUR ENTERTAINMENT:</h2>
+      <h2 className="text-4xl font-extrabold text-white ">EXPLORE OUR PROGRAMME ON MEDIA SOCIAL</h2>
       </div>
       
       </div>
       <div className="h-full lg:py-7 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4 items-center">
         <a href="https://www.tiktok.com/@yegacademy/video/7314661907741428993">
-        <video autoPlay 
-                  loop 
-                  muted>
+        <video autoPlay loop muted playsInline
+                  className="rounded-md  top-0 left-0 w-full h-full object-cover" 
+                  width="390" 
+                  height="890">
               <source src="/tiktok1.mp4" type="video/mp4"/>
           </video></a>
       
@@ -97,7 +96,7 @@ function Intro() {
           <div className="bg-yellow-600 w-1/2 rounded-l-md">d</div>
           </div>
       </div>
-    </section11>
+    </section>
     </>
     
   )
