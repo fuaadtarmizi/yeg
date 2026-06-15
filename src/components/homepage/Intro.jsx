a7fd6f183554740acce174f8bfec79c14ce27044
Dina <ndina1943@gmail.com>
2026-06-15 17:01:58 +0800

    Fix

--- src/components/homepage/Intro.jsx
+++ src/components/homepage/Intro.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Navbar from '@/components/Navbar'
+import Sidebar from '@/components/Sidebar'
 
 function Intro() {
   return (
     <>
+      {/* <Sidebar/>
+    <Navbar/ className="bg-gradient-to-r from-[#151c41] to-[rgba(11,24,54,0)"> */}
       <section className="relative">
         <div className="absolute top-0 left-0 h-full w-1/4"></div>
         <div className="bg-black max-w-full lg:px-3 flex justify-center">
