0480dd33ffc67814506c9150e408eaab82f2b59f
Dina <ndina1943@gmail.com>
2026-06-29 16:09:55 +0800

    update

--- src/components/homepage/Mainstream2.jsx
+++ src/components/homepage/Mainstream2.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Image from "next/image"
+
 
 
 
@@ -10,7 +10,7 @@ function Intro() {
     <div className="py-8 lg:py-10 md:[height:80px]"></div>
     <div className="w-full flex justify-center bg-slate-100">
         <span className="lg:text-4xl font text-4xl text-blue-950 font-bold text-center"><span className="text-yellow-600">YEG ACADEMY</span> IN MAINSTREAM MEDIA</span>
-      </div>
+      
       <div className="w-full h-full grid  lg:grid-cols-12 gap-8 md:col-span-2 py-10 p-2 bg-slate-100">
       <div className="flex justify-center w-full h-full mx-auto col-span-12 md:col-span-6 lg:col-span-6 md:order-2">
         <video
@@ -24,6 +24,7 @@ function Intro() {
         <source src="/MHI.mp4" type="video/mp4" />
         </video>
         </div>
+        </div>
 
         <div className="flex justify-center w-full h-full mx-auto col-span-12 md:col-span-6 lg:col-span-6 md:order-2">
             <video 
@@ -32,12 +33,13 @@ function Intro() {
             playsInline 
             autoPlay
             muted
+            crossOrigin="anonymous"
           >
             <source src="/NLKO.mp4" type="video/mp4" />
           </video>
         </div>  
       </div>
-      <div className="py-8 lg:py-10 md:[heig`ht:80px]"></div>
+      <div className="py-8 lg:py-10 md:[height:80px]"></div>
     </section>
     </>
     
