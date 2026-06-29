f1eb74a7d62e485d2bcbbcdfa42c57082825459d
Dina <ndina1943@gmail.com>
2026-06-29 16:34:01 +0800

    update

--- src/components/homepage/Mainstream2.jsx
+++ src/components/homepage/Mainstream2.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
 
-
-
-
 function Intro() {
   return (
     <>
@@ -10,30 +7,30 @@ function Intro() {
     <div className="py-8 lg:py-10 md:[height:80px]"></div>
     <div className="w-full flex justify-center bg-slate-100">
         <span className="lg:text-4xl font text-4xl text-blue-950 font-bold text-center"><span className="text-yellow-600">YEG ACADEMY</span> IN MAINSTREAM MEDIA</span>
-      
+      </div>
       <div className="w-full h-full grid  lg:grid-cols-12 gap-8 md:col-span-2 py-10 p-2 bg-slate-100">
       <div className="flex justify-center w-full h-full mx-auto col-span-12 md:col-span-6 lg:col-span-6 md:order-2">
         <video
-        className="w-full h-auto"
+        className="w-[1000px] h-auto"
         controls
         playsInline
         autoPlay
         muted
-        crossOrigin="anonymous"
+      
     >
         <source src="/MHI.mp4" type="video/mp4" />
         </video>
-        </div>
+        
         </div>
 
         <div className="flex justify-center w-full h-full mx-auto col-span-12 md:col-span-6 lg:col-span-6 md:order-2">
             <video 
-            className="w-full h-auto"
+            className="w-[1000px] h-auto"
             controls 
             playsInline 
             autoPlay
             muted
-            crossOrigin="anonymous"
+        
           >
             <source src="/NLKO.mp4" type="video/mp4" />
           </video>
